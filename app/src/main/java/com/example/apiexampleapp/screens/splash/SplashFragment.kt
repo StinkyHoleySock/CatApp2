7be70c96d73a4fdbe1bd1dbca5fe3cf9c120c8ea
dmitry <piervov00@gmail.com>
2022-03-18 13:55:25 +0300

    - change icon - add MotionLayout

--- app/src/main/java/com/example/apiexampleapp/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/example/apiexampleapp/screens/splash/SplashFragment.kt
@@ -5,14 +5,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.apiexampleapp.R
+import kotlinx.android.synthetic.main.fragment_splash.*
 
 class SplashFragment: Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
-        }, 3000)
+
+            view.postDelayed({
+                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)},
+                2500)
     }
+
 }
